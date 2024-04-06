fcca2fd0e045c6ff44183b3e12602e84ca74b762
Darima98 <165848832+Darima98@users.noreply.github.com>
2024-04-06 20:43:12 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 5, False, "String"
+print(immutable_var)
+tuple[1] = 100 #кортеж не поддерживает обращение по элементам
+mutable_list = ['Дом', 'Крыша', 'Дверь']
+mutable_list[0] = 'Дача'
+mutable_list[2] = 'Окно'
+print(mutable_list)
